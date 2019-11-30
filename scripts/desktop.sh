18f666ff4327eff94310f31fccf45bca7f2859fe
Jeff Schnurr <jschnurr@gmail.com>
2019-11-30 09:10:05 -0500

    add terminal padding to gtk

--- scripts/desktop.sh
+++ scripts/desktop.sh
@@ -2,9 +2,14 @@
 
 # Set up the desktop
 gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
-gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-from 21.0
+gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-from 23.0
 gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-to 6.0
 gsettings set org.gnome.desktop.session idle-delay 900
 gsettings set org.gnome.desktop.screensaver lock-enabled true
 gsettings set org.gnome.desktop.screensaver lock-delay 60
 gsettings set org.gnome.desktop.screensaver show-notifications false
+
+# Padding around terminal
+echo "vte-terminal {
+    padding: 10px;
+}" >> ~/.config/gtk-3.0/gtk.css
